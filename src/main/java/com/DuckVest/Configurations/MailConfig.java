65f223dcc2fd9f0998e8fa41b8f851c8491bdaa8
Digitalturkk <huseynovfarid1111@gmail.com>
2025-07-02 01:05:53 +0400

    Add stock-level diversification analysis with HHI and risk rating
    - Implemented `calculateStockDiversification()` method to assess portfolio concentration by individual stocks.
    - Calculated HHI-based diversification score and added qualitative rating (Excellent, Good, Medium, High Risk).
    - Created `StockDiversificationDTO` to expose symbol-based percentage allocations, diversification score, and level.

---
+++ src/main/java/com/DuckVest/Configurations/MailConfig.java
@@ -0,0 +1,43 @@
+package com.DuckVest.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSenderImpl mailSender() throws Exception {
+        // Отключаем SSL-проверку
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("duckvest.inc@gmail.com");
+        mailSender.setPassword("hylwoimyujerdspz");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "*");
+
+        return mailSender;
+    }
+}
